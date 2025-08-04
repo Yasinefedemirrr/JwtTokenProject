e739e8ebdc29fd837f33237203f50a79706cb8da
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-04 14:08:53 +0300

    Düzenlemeler yapıldı

--- JWTAPI/Program.cs
+++ JWTAPI/Program.cs
@@ -1,12 +1,14 @@
 ﻿using JWT.Application.Features.CQRS.Handlers.CityWeatherHandlers;
+using JWT.Application.Features.CQRS.Handlers.Districthandlers;
 using JWT.Application.Interfaces;
+using JWT.Application.Tools;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Persistance.Context;
 using Persistance.Repositories;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using JWT.Application.Tools;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,16 +35,82 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 
+builder.Services.AddScoped<GetCityWeatherQueryHandler>();
+builder.Services.AddScoped<GetCityWeatherByIdQueryHandler>();
+builder.Services.AddScoped<CreateCityWeatherCommandHandler>();
+builder.Services.AddScoped<UpdateCityWeatherCommandHandler>();
+builder.Services.AddScoped<RemoveCityWeatherCommandHandler>();
+
+builder.Services.AddScoped<GetDistrictQueryHandler>();
+builder.Services.AddScoped<GetDistrictByIdQueryHandler>();
+builder.Services.AddScoped<CreateDistrictCommandHandlers>();
+builder.Services.AddScoped<UpdateDistrictCommandHandlers>();
+builder.Services.AddScoped<RemoveDistrictCommandHandlers>();
+
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 
 builder.Services.AddHttpClient();
+
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "JWT API",
+        Version = "v1"
+    });
+
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "JWT Bearer token giriniz. Örn: Bearer {token}",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT"
+    };
+
+    c.AddSecurityDefinition("Bearer", securityScheme);
+
+    var securityRequirement = new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    };
+
+    c.AddSecurityRequirement(securityRequirement);
+});
 
 var app = builder.Build();
 
 
+app.Use(async (context, next) =>
+{
+    if (context.Request.Cookies.ContainsKey("JWTToken"))
+    {
+        var token = context.Request.Cookies["JWTToken"];
+        if (!string.IsNullOrEmpty(token))
+        {
+            context.Request.Headers["Authorization"] = $"Bearer {token}";
+        }
+    }
+    await next();
+});
+
+// Swagger UI
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -51,9 +119,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
 app.Run();
