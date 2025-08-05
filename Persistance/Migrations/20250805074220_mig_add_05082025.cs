f5e73fa1ec3dff1aad6cb1cfd1a1b9f34c72431f
Yasinefedemirrr <120326283+Yasinefedemirrr@users.noreply.github.com>
2025-08-05 11:24:25 +0300

    Migration

---
+++ Persistance/Migrations/20250805074220_mig_add_05082025.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_add_05082025 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "AppUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpireDate",
+                table: "AppUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "AppUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpireDate",
+                table: "AppUsers");
+        }
+    }
+}
